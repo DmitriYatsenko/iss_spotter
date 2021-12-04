db806a7d7e280f636a76dab000063f8c2788f8d9
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-04 03:11:12 +0000

    coord-n8s by ip

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const { fetchMyIP, fetchCoordsByIp } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const request = require('request');
 
 fetchMyIP((error, ip) => {
     if (error) {
         console.log("It didn't work!", error);
         return;
     }
-
     console.log('It worked! Returned IP:', ip);
 });
-
-fetchCoordsByIp((error, coordin8s) => {
+/*
+fetchCoordsByIP((error, coordin8s) => {
     if (error) {
         console.log("It didn't work!", error);
         return;
     }
 });
+*/
--- iss.js
+++ iss.js
@@ -12,29 +12,56 @@ const fetchMyIP = function (callback) {
             return;
         } else {
             const ip = JSON.parse(body).ip;
+            fetchCoordsByIP(ip, (error, coordinates) => {
+                if (error) {
+                    console.log("It didn't work!", error);
+                    return;
+                }
+                console.log('It worked! Returned coordinates:', coordinates);
+            });
             return callback(null, ip);
         }
     });
 }
-
-const fetchCoordsByIp = function (callback) {
+/*
+const fetchCoordsByIP = function (callback) {
     request(`https://api.freegeoip.app/json/?apikey=0b8ab680-549a-11ec-9627-63e5cec2c145`, (error, response, body) => {
+    request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
         if (error) {
             callback(error, null);
             return;
         }
         if (response.statusCode !== 200) {
-            const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+            const msg = `Status Code ${response.statusCode} when fetching coord-n8s 4 IP. Response: ${body}`;
             callback(Error(msg), null);
             return;
         } else {
             const latitude = JSON.parse(body).latitude;
             const longitude = JSON.parse(body).longitude;
-            const coordin8s = [latitude, longitude];
+            const coordin8s = {}; //[latitude, longitude];
+            coordin8s['latitude'] = latitude;
+            coordin8s['longitude'] = longitude;
             console.log(coordin8s);
             return callback(null, coordin8s);
         }
     });
 }
+*/
+const fetchCoordsByIP = function (ip, callback) {
+    request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+            return;
+        }
 
-module.exports = { fetchMyIP, fetchCoordsByIp };
+        if (response.statusCode !== 200) {
+            callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+            return;
+        }
+
+        const { latitude, longitude } = JSON.parse(body);
+        callback(null, { latitude, longitude });
+    });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
