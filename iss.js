793029be5aea5cf6455f442bb14ddf25192673a4
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-04 05:44:15 +0000

    flyover times

--- iss.js
+++ iss.js
@@ -12,12 +12,12 @@ const fetchMyIP = function (callback) {
             return;
         } else {
             const ip = JSON.parse(body).ip;
-            fetchCoordsByIP(ip, (error, coordinates) => {
+            fetchCoordsByIP(ip, (error, coordin8s) => {
                 if (error) {
                     console.log("It didn't work!", error);
                     return;
                 }
-                console.log('It worked! Returned coordinates:', coordinates);
+                console.log('It worked! Returned coordin8s:', coordin8s);
             });
             return callback(null, ip);
         }
@@ -37,14 +37,38 @@ const fetchCoordsByIP = function (ip, callback) {
         } else {
             const latitude = JSON.parse(body).latitude;
             const longitude = JSON.parse(body).longitude;
-            const coordin8s = {}; //[latitude, longitude];
+            const coordin8s = {};
             coordin8s['latitude'] = latitude;
             coordin8s['longitude'] = longitude;
             //console.log(coordin8s);
+            fetchISSFlyOverTimes(coordin8s, (error, flyoverTimes) => {
+                if (error) {
+                    console.log("It didn't work!", error);
+                    return;
+                }
+                console.log('It worked! Returned flyover times:', flyoverTimes);
+            });
             return callback(null, coordin8s);
         }
     });
 }
+
+const fetchISSFlyOverTimes = function (coordin8s, callback) {
+    request(`https://iss-pass.herokuapp.com/json/?lat=${coordin8s.latitude}&lon=${coordin8s.longitude}`, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        if (response.statusCode !== 200) {
+            const msg = `Status Code ${response.statusCode} when fetching flyover times 4 loc8n coord-n8s. Response: ${body}`;
+            callback(Error(msg), null);
+            return;
+        } else {
+            const flyoverTimes = JSON.parse(body).response;
+            return callback(null, flyoverTimes);
+        }
+    });
+};
 
 /*
 const fetchCoordsByIP = function (ip, callback) {
